5a11ffae91228dcd4ee582b553fd485c1a640ebc
Vitalii-Karashchuk <83291711+Vitalii-Karashchuk@users.noreply.github.com>
2024-09-14 18:11:53 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,16 +6,18 @@ const inputEl = document.querySelector('input');
 const messageEl = document.querySelector('textarea');
 let formEl = document.querySelector('.feedback-form');
 const localStorageKey = "feedback-form-state";
-if(localStorage.getItem(localStorageKey) !== null){
-    const parse = JSON.parse(localStorage.getItem(localStorageKey));
-    inputEl.value = parse.email;
-    messageEl.value = parse.message;
-    // console.log(parse);
-}
-
-const handleInputForm = event => {
-    localStorage.setItem(localStorageKey, event.target.value);
-    };
+function dataForm (){
+    if(localStorage.getItem(localStorageKey) !== null){
+        const parse = JSON.parse(localStorage.getItem(localStorageKey));
+        inputEl.value = parse.email;
+        messageEl.value = parse.message;
+        console.log(parse);
+    }
+};    
+  const handleInputForm = event => {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+};
 
 const handleForm = event => {
     event.preventDefault();
@@ -26,14 +28,18 @@ const handleForm = event => {
     if (email.value && message.value) {
         formData.email = email.value;
         formData.message = message.value;
-        localStorage.setItem(localStorageKey, JSON.stringify(formData));      
-        form.reset();
+        localStorage.setItem(localStorageKey, JSON.stringify(formData)); 
+        form.reset();  
+        console.log(formData);
+        localStorage.removeItem(localStorageKey);
     } else {
       alert('Fill please all fields.');
-    }
+    } 
+    
+    
  };
 
-localStorage.removeItem(localStorageKey);
-formEl.addEventListener('input', handleInputForm);
 formEl.addEventListener('submit', handleForm);
-// console.log(formData);
+formEl.addEventListener('input', handleInputForm);
+dataForm ();
+
